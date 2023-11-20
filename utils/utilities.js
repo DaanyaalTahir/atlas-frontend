764960d12d9be66fb735f917b2ea45eb853585b4
Daanyaal Tahir <tahir.daanyaal@gmail.com>
2023-11-20 14:24:09 -0500

    Fix image problem

--- utils/utilities.js
+++ utils/utilities.js
@@ -1,8 +1,13 @@
 export const getTrackerIcon = (itemType) => {
+  console.log(itemType);
   switch (itemType) {
     case "car":
       return require("../assets/car.png");
     case "bag":
       return require("../assets/backpack.png");
+    case "person":
+      return require("../assets/person.png");
+    default:
+      return require("../assets/default-placeholder.png");
   }
 };
